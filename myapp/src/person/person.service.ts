65a538d338c5359af431528ef970eedb81429b5f
CyRa89 <90359416+CyRa89@users.noreply.github.com>
2021-10-18 18:50:24 -0500

    Servicio de la entidad Persona

--- myapp/src/person/person.service.ts
+++ myapp/src/person/person.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePersonDto } from './dto/create-person.dto';
@@ -24,13 +24,47 @@ export class PersonService {
           data
         };
   }
+  async findAll() {
+    const data = await this.personRepository.find();
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+    return {
+      code : "OK",
+      message : "Consulta realizada con éxito",
+      data
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async findOne(id: number) {
+    const data = await this.personRepository.findOne(id);
+
+    return {
+      code : "OK",
+      message : "Consulta realizada con éxito",
+      data
+    };
+  }
+
+  async update(id: number, updatePersonDto: UpdatePersonDto) {
+    const person = await this.personRepository.findOne(id);
+    if (!person) throw new NotFoundException("Libro inexistente");
+    const editedperson = Object.assign(person, updatePersonDto)
+    const data = await this.personRepository.save(editedperson);
+    return {
+      code : "OK",
+      message : "Consulta realizada con éxito",
+      data
+    };  
+  }
+
+  async remove(id: number) {
+    const person = await this.personRepository.findOne(id);
+      if (!person) throw new NotFoundException("Persona inexistente");
+      const data = this.personRepository.remove(person);
+      return {
+        code : "OK",
+        message : "Consulta realizada con éxito",
+        data
+      };
   }
 }
     function person(person: any) {
